d684ab9f43725c3b7294f5329f45f3e8817f9998
Juan Pablo Senn <juanpsenn@gmail.com>
2021-09-26 17:26:11 -0300

    Root now redirects to the docs page

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
 from v1.routes import status
 
 app = FastAPI()
 app.include_router(status.router)
+
+
+@app.get("/")
+async def root():
+    response = RedirectResponse(url='/docs')
+    return response
